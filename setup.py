629ab4cb404a41e6c5331434db9924eb5a1542d6
DrEggman399 <5958211+DrEggman399@users.noreply.github.com>
2025-01-16 16:41:47 -0500

    Prepare for build with cx_freeze

---
+++ setup.py
@@ -0,0 +1,9 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="Linux Steam Shortcut Helper",
+    version="0.1",
+    description="A small PyQt helper application for configuring and quickly adding Non-Steam Shortcuts ",
+    url="https://github.com/DrEggman399/LinuxSteamShortcutHelper",
+    executables=[Executable("main.py", base="gui")],
+)
