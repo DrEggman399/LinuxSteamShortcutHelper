5b2d0668fd904ab084d4489cbbbcfb5398cfb884
DrEggman399 <5958211+DrEggman399@users.noreply.github.com>
2025-02-28 21:28:12 -0500

    Bump release version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name="Linux Steam Shortcut Helper",
-    version="0.1.1",
+    version="0.1.2",
     description="A small PyQt helper application for configuring and quickly adding Non-Steam Shortcuts ",
     url="https://github.com/DrEggman399/LinuxSteamShortcutHelper",
     executables=[Executable("main.py", base="gui")],
